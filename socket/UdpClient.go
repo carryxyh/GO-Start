f09e330a349f8dbdacc7fa2b068c54170d2693cf
ziyuan <ziyuan@2dfire.com>
2017-03-04 16:30:47 +0800

    udp

---
+++ socket/UdpClient.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+	service := ":7777"
+	udpAddr, err := net.ResolveUDPAddr("udp4", service)
+	checkError(err)
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	checkError(err)
+	_, err = conn.Write([]byte("anything"))
+	checkError(err)
+	var buf [512]byte
+	n, err := conn.Read(buf[0:])
+	checkError(err)
+	fmt.Println(string(buf[0:n]))
+	os.Exit(0)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
