84738246c919fe4c340e708729f3a3ca6e4b78c4
ziyuan <ziyuan@2dfire.com>
2017-04-10 20:55:19 +0800

    test

--- TestInitMethod.go
+++ TestInitMethod.go
@@ -8,6 +8,23 @@ func init() {
 	fmt.Println("init")
 }
 
+type DBconfig struct {
+	Alias      string
+	DriverName string
+	DataSource string
+	Conns      []int
+}
+
 func main() {
+	var c *DBconfig
+	//fmt.Println(c.Alias)  <- 这里报错
+	fmt.Println(&c)
+
+	// cc := DBconfig{}
+
+	fmt.Println(new(DBconfig))
 	fmt.Println("main...")
+
+	t := DBconfig{}
+	fmt.Println(&t)
 }
